caf1c3064fa138a3e035baebc9fe8591882b5f58
nazar-dmytrenko <nazarkadm@mail.com>
2026-02-23 16:48:45 +0200

    solution v2

--- init.sql
+++ init.sql
@@ -1,2 +1,2 @@
 -- Use the user that is running init script (POSTGRES_USER from .env).
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO POSTGRES_USER;
